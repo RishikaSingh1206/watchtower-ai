292aae0ff6ae9349b842070e03271c61c7ad3667
Akshy <akshyansunayak360985@gmail.com>
2024-06-12 23:10:08 +0530

    final changes

--- utils/draw.ts
+++ utils/draw.ts
@@ -1,40 +1,36 @@
-import * as cocossd from '@tensorflow-models/coco-ssd'
-import "@tensorflow/tfjs-backend-cpu"
-import "@tensorflow/tfjs-backend-webgl"
-import { DetectedObject } from '@tensorflow-models/coco-ssd';
-// mirrored, predictions, canvasRef.current?.getContext('2d')/
+import { DetectedObject } from "@tensorflow-models/coco-ssd";
+
+// mirrored, predictions, canvasRef.current?.getContext('2d')
 export function drawOnCanvas(
-    mirrored: boolean,
-    predictions : DetectedObject[],
-    ctx: CanvasRenderingContext2D | null |undefined
-){
-  predictions.forEach((detectedObject: DetectedObject)=>{
-    const {class:nameoftheobject, bbox, score} = detectedObject;
-    const [x,y,width, height] = bbox;
+  mirrored: boolean,
+  predictions: DetectedObject[],
+  ctx: CanvasRenderingContext2D | null | undefined
+) {
+  predictions.forEach((detectedObject: DetectedObject) => {
+    const { class: name, bbox, score } = detectedObject;
+    const [x, y, width, height] = bbox;
 
     if (ctx) {
-        ctx.beginPath();
+      ctx.beginPath();
 
+      // styling
+      ctx.fillStyle = name === "person" ? "#FF0F0F" : "#00B612";
+      ctx.globalAlpha = 0.4;
 
-        //Styleing
+      mirrored
+        ? ctx.roundRect(ctx.canvas.width - x, y, -width, height, 8)
+        : ctx.roundRect(x, y, width, height, 8);
 
-        ctx.fillStyle = nameoftheobject === 'person' ? '#FF0F00' : ' #00B612'
-        ctx.globalAlpha = 0.4;
-        
-        
-        
-        ctx.roundRect(x,y,width, height, 8);
-        
-        
-        ctx.fill()
-        ctx.font = " 12px Courier New"
+      // draw stroke or fill
+      ctx.fill();
 
-        ctx.globalAlpha = 1;
-
-
-        ctx.fillText(nameoftheobject, x, y)
-        
+      // text styling
+      ctx.font = "12px Courier New";
+      ctx.fillStyle = 'black'
+      ctx.globalAlpha = 1;
+      mirrored
+        ? ctx.fillText(name, ctx.canvas.width - x -width + 10, y + 20)
+        : ctx.fillText(name, x + 10 , y + 20);
     }
-  })
-
+  });
 }
